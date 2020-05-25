c9c4f688237c10c2866836a49d283b066ca697c6
lucienmaloney <lucienmaloney@student.lsmsa.edu>
2020-05-25 13:33:05 -0500

    Improved documentation and organization

--- BeatTimer/Program.cs
+++ BeatTimer/Program.cs
@@ -5,18 +5,13 @@ using FFTWSharp;
 
 namespace BeatTimer
 {
-    class Program
+    class BeatTimer
     {
         static void Main(string[] args)
         {
             double[] data;
-            readWav(args[0], out data);
+            WAV.readWav(args[0], out data);
             var spec = spectrogram(data, 2048, 128);
-            //var fft = FFT(x);
-            //for (int i = 0; i < 2048; i++)
-            //{
-            //   Console.WriteLine(fft[i]);
-            //}
             Console.WriteLine(spec.Length);
             for (int i = 0; i < 10; i++)
             {
@@ -24,15 +19,29 @@ namespace BeatTimer
             }
         }
 
-        // https://www.youtube.com/watch?v=9SwuRRe-2Jk
+        /// <summary>
+        ///   Calculates the spectral flux of a real double array
+        ///   <example>
+        ///     <code>
+        ///       var specflux = spectrogram(somedata, 4096, 1024);
+        ///     </code>
+        ///   </example>
+        /// </summary>
+        /// <param name="arr">Any real data</param>
+        /// <param name="size">FFT length (power of 2)</param>
+        /// <param name="step">How much to increment for start of following FFT</param>
+        /// <returns>Array reprenting change in sum of frequencies over time</returns>
         static double[] spectrogram(double[] arr, int size, int step)
         {
             int n = arr.Length;
+            // FFT of real values is symmetric, so we only need an array half the length + 1
             int fftlength = size / 2 + 1;
+            // The number of groupings of size shifted step apart you can fit in n, minus 10
             int speclength = (n - size + step) / step - 10;
 
             double[] spec = new double[speclength];
             double[,] ffts = new double[11,fftlength];
+            // FFT is returned as complex number with every other index being imaginary, so double length
             double[] fft = new double[fftlength * 2];
 
             IntPtr ptr = fftw.malloc(size * sizeof(double));
@@ -47,6 +56,7 @@ namespace BeatTimer
                 Marshal.Copy(ptrout, fft, 0, fftlength * 2);
                 for (int j = 0; j < fftlength * 2; j += 2)
                 {
+                    // Set to absolute value of complex number
                     ffts[index % 11, j / 2] = Math.Sqrt(fft[j] * fft[j] + fft[j + 1] * fft[j + 1]);
                 }
                 if (index >= 10)
@@ -54,6 +64,7 @@ namespace BeatTimer
                     for (int j = 0; j < fftlength; j++)
                     {
                         double diff = ffts[index % 11, j] - ffts[(index - 10) % 11, j];
+                        // Only add difference if positive. Results in better results
                         spec[index - 10] += diff > 0 ? diff : 0;
                     }
                 }
@@ -63,15 +74,18 @@ namespace BeatTimer
             fftw.cleanup();
             return spec;
         }
+    }
 
-        // https://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
+    // https://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
+    class WAV
+    {
         static double bytesToDouble(byte firstByte, byte secondByte)
         {
             short s = (short)((secondByte << 8) | firstByte);
             return s / 32768.0;
         }
 
-        static void readWav(string filename, out double[] audio)
+        public static void readWav(string filename, out double[] audio)
         {
             byte[] wav = File.ReadAllBytes(filename);
             int channels = wav[22];
