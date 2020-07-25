e066228c3da912a0295bd323399e2eb4fca7abdb
Lucien Maloney <hobbesdevelopment@gmail.com>
2020-07-25 13:22:26 -0500

    New FP data generation

---
+++ FP.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace BeatTimer {
+    public struct FP
+    {
+        public FP(double time, int f, int p)
+        {
+            T = time;
+            F = f;
+            P = p;
+        }
+
+        public FP(double time, double f, double p) {
+            T = time;
+            F = f < 350 ? 0 : f < 1000 ? 1 : 2;
+            P = p < 5000 ? 0 : p < 8000 ? 1 : 2;
+        }
+
+        public double T { get; }
+        public int F { get; }
+        public int P { get; }
+
+        public override string ToString()
+        {
+            return $"Time: {T,10:0.00}, Freneticism: {F,10}, Physicality: {P,10}";
+        }
+    }
+
+    class BeatParser {
+        public static List<FP> FPdata(List<Beat> beats) {
+            var fp = new List<FP>();
+            for (int i = 0; i + 64 <= beats.Count; i += 64) {
+                double p = 0;
+                double f = 0;
+                double time = beats[i].T;
+                double prev = 0;
+
+                for (int j = 0; j < 64; j++) {
+                    p += Math.Sqrt(beats[i + j].I);
+                    f += Math.Abs(beats[i + j].P - prev);
+                    prev = beats[i + j].P;
+                }
+
+                fp.Add(new FP(time, f, p));
+            }
+            return fp;
+        }
+    }
+}
--- Index.cs
+++ Index.cs
@@ -6,11 +6,13 @@ namespace BeatTimer
     {
         static void Main(string[] args)
         {
-            var beats = BeatTimer.beatdata(args[0]);
             Console.WriteLine($"Getting beat data for {args[0]}...");
-            foreach (var beat in beats)
+            var beats = BeatTimer.beatdata(args[0]);
+            var fpdata = BeatParser.FPdata(beats);
+            
+            foreach (var fp in fpdata)
             {
-                Console.WriteLine(beat);
+                Console.WriteLine(fp);
             }
         }
     }
